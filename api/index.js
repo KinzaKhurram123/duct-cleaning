293a5cac93a4909331ab48d4b25478314a674ca7
KinzaKhurram123 <kinzakhurram84@gmail.com>
2026-05-18 17:24:01 +0500

    changes

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const Booking = require('./models/Booking');
+const Booking = require('../models/Booking');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
