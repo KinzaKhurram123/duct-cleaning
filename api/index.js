0c421ae1d51655c858ae17d62f4eb260d8d9f596
KinzaKhurram123 <kinzakhurram84@gmail.com>
2026-05-18 21:48:53 +0500

    changes

--- api/index.js
+++ api/index.js
@@ -10,15 +10,12 @@ const Booking = require('./models/Booking');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files (your HTML)
-  // app.use(express.static('public'));
+  app.use(express.static('public'));
 
-// MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
